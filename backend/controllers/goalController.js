e8e23e2b6dddb38de7c856fe2230d4a3f58867a9
So Youn Choi <manghatuhyo@gmail.com>
2023-12-17 13:21:20 +0100

    was not able to proceed after 41:43 at video, due to not able to connect the database via mongodb compass

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler')
 
 // @desc    Get goals
 // @route   GET /api/goals
-// @access  Private
+// @access  Private.
 const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
 })
